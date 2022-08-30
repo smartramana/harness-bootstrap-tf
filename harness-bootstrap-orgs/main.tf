350ec561f729b8e37608b511f00a7029ce9bd4eb
root <cristiano.rosetti@gmail.com>
2022-08-30 01:33:58 +0000

    orgs, projs, github connector and secrets setup

--- harness-bootstrap-orgs/main.tf
+++ harness-bootstrap-orgs/main.tf
@@ -1,6 +1,18 @@
-module "bootstrap_project" {
+module "bootstrap_harness" {
   source                    = "git::https://github.com/crizstian/harness-terraform-modules.git//harness-project?ref=main"
-  harness_platform_projects = var.harness_platform_projects
+  harness_platform_projects = local.harness_platform_projects
+
+  providers = {
+    harness = harness.provisioner
+  }
+}
+
+module "bootstrap_harness_connectors" {
+  depends_on = [
+    module.bootstrap_harness
+  ]
+  source                      = "git::https://github.com/crizstian/harness-terraform-modules.git//harness-connectors?ref=main"
+  harness_platform_connectors = local.harness_platform_connectors
 
   providers = {
     harness = harness.provisioner
@@ -9,6 +21,7 @@ module "bootstrap_project" {
 
 output "details" {
   value = {
-    project = module.bootstrap_project.project
+    project      = module.bootstrap_harness.project
+    organization = module.bootstrap_harness.organization
   }
 }

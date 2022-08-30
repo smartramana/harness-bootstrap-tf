e92137c2d21cf6e817d5a5beac5a48703961824d
root <cristiano.rosetti@gmail.com>
2022-08-30 05:00:59 +0000

    add secrets

---
+++ harness-bootstrap-orgs/harness-secrets.tf
@@ -0,0 +1,17 @@
+resource "harness_platform_secret_text" "harness_secrets" {
+  for_each                  = local.harness_platform_secrets
+  identifier                = lower(replace(each.key, "/[\\s-.]/", "_"))
+  name                      = each.key
+  description               = each.value.description
+  secret_manager_identifier = "harnessSecretManager"
+  value_type                = "Inline"
+  value                     = each.value.secret
+  org_id                    = each.value.org_id
+  project_id                = each.value.project_id
+
+  lifecycle {
+    ignore_changes = [
+      value,
+    ]
+  }
+}

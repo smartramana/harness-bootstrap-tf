00d3ab357dc640a9be1f0deae3b530a830880609
root <cristiano.rosetti@gmail.com>
2022-08-30 04:06:51 +0000

    add delegate to org

--- harness-bootstrap-orgs/main.tf
+++ harness-bootstrap-orgs/main.tf
@@ -7,9 +7,16 @@ module "bootstrap_harness" {
   }
 }
 
+module "bootstrap_harness_delegates" {
+  source                     = "git::https://github.com/crizstian/harness-terraform-modules.git//harness-delegate?ref=main"
+  harness_platform_delegates = local.harness_platform_delegates
+  harness_platform_api_key   = var.harness_platform_api_key
+}
+
 module "bootstrap_harness_connectors" {
   depends_on = [
-    module.bootstrap_harness
+    module.bootstrap_harness,
+    module.bootstrap_harness_delegates
   ]
   source                      = "git::https://github.com/crizstian/harness-terraform-modules.git//harness-connectors?ref=main"
   harness_platform_connectors = local.harness_platform_connectors
@@ -21,7 +28,8 @@ module "bootstrap_harness_connectors" {
 
 output "details" {
   value = {
-    project      = module.bootstrap_harness.project
     organization = module.bootstrap_harness.organization
+    delegates    = module.bootstrap_harness_delegates.delegates
+    project      = module.bootstrap_harness.project
   }
 }

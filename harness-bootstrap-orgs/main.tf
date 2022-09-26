60ce903e74d2fba8d1c9fa842b12536c89d5062d
root <cristiano.rosetti@gmail.com>
2022-09-26 03:14:49 +0000

    refactor to only account resource creation

--- harness-bootstrap-orgs/main.tf
+++ harness-bootstrap-orgs/main.tf
@@ -1,23 +1,20 @@
 module "bootstrap_harness_account" {
-  source                    = "git::https://github.com/crizstian/harness-terraform-modules.git//harness-project?ref=main"
-  harness_platform_projects = local.harness_platform_projects
+  source                         = "git::https://github.com/crizstian/harness-terraform-modules.git//harness-project?ref=main"
+  harness_platform_organizations = local.harness_platform_organizations
 
   providers = {
     harness = harness.provisioner
   }
 }
 
-module "bootstrap_harness_connector" {
+module "bootstrap_harness_delegates" {
   depends_on = [
     module.bootstrap_harness_account,
-    # module.bootstrap_harness_delegates
   ]
-  source                      = "git::https://github.com/crizstian/harness-terraform-modules.git//harness-connectors?ref=main"
-  harness_platform_connectors = local.harness_platform_connectors
-
-  providers = {
-    harness = harness.provisioner
-  }
+  source                     = "git::https://github.com/crizstian/harness-terraform-modules.git//harness-delegate?ref=main"
+  harness_platform_delegates = local.harness_platform_delegates
+  harness_platform_api_key   = var.harness_platform_api_key
+  harness_account_id         = var.harness_platform_account_id
 }
 
 resource "harness_platform_secret_text" "harness_secrets" {
@@ -31,7 +28,6 @@ resource "harness_platform_secret_text" "harness_secrets" {
   secret_manager_identifier = "harnessSecretManager"
   value_type                = "Inline"
   value                     = each.value.secret
-  org_id                    = each.value.org_id
 
   lifecycle {
     ignore_changes = [
@@ -40,32 +36,35 @@ resource "harness_platform_secret_text" "harness_secrets" {
   }
 }
 
-module "bootstrap_harness_templates" {
-  depends_on = [
-    module.bootstrap_harness_account,
-  ]
-  source                                 = "git::https://github.com/crizstian/harness-terraform-modules.git//harness-templates?ref=main"
-  harness_platform_api_key               = var.harness_platform_api_key
-  harness_templates                      = local.harness_templates
-  harness_template_endpoint_account_args = local.harness_template_endpoint_account_args
-}
+# module "bootstrap_harness_connector" {
+#   depends_on = [
+#     module.bootstrap_harness_account,
+#     # module.bootstrap_harness_delegates
+#   ]
+#   source                      = "git::https://github.com/crizstian/harness-terraform-modules.git//harness-connectors?ref=main"
+#   harness_platform_connectors = local.harness_platform_connectors
 
-module "bootstrap_harness_delegates" {
-  depends_on = [
-    module.bootstrap_harness_account,
-  ]
-  source                     = "git::https://github.com/crizstian/harness-terraform-modules.git//harness-delegate?ref=main"
-  harness_platform_delegates = local.harness_platform_delegates
-  harness_platform_api_key   = var.harness_platform_api_key
-  harness_account_id         = var.harness_platform_account_id
-}
+#   providers = {
+#     harness = harness.provisioner
+#   }
+# }
+
+# module "bootstrap_harness_templates" {
+#   depends_on = [
+#     module.bootstrap_harness_account,
+#   ]
+#   source                                 = "git::https://github.com/crizstian/harness-terraform-modules.git//harness-templates?ref=main"
+#   harness_platform_api_key               = var.harness_platform_api_key
+#   harness_templates                      = local.harness_templates
+#   harness_template_endpoint_account_args = local.harness_template_endpoint_account_args
+# }
 
 output "details" {
   value = {
     organization = module.bootstrap_harness_account.organization
     delegates    = module.bootstrap_harness_delegates.delegates
-    project      = module.bootstrap_harness_account.project
-    connectors   = module.bootstrap_harness_connector.connectors
+    # project      = module.bootstrap_harness_account.project
+    # connectors   = module.bootstrap_harness_connector.connectors
     # manifests  = module.bootstrap_harness_delegates.manifests
   }
 }
--- harness-bootstrap-orgs/variables.tf
+++ harness-bootstrap-orgs/variables.tf
@@ -4,29 +4,15 @@ variable "harness_platform_api_key" {
 variable "harness_platform_account_id" {
   sensitive = true
 }
-variable "harness_connector_crizstian_github_token" {
-  sensitive = true
-}
-variable "harness_connector_crizstian_docker_token" {
-  sensitive = true
-}
-variable "harness_connector_crizstian_artifactory_token" {
-  sensitive = true
-}
 
 locals {
-  harness_platform_projects = tomap({
-    "cristian-lab-org" = var.cristian_lab_org_projects
-  })
-  harness_platform_connectors = merge(
-    local.cristiab_lab_org_connectors
-  )
-  harness_platform_delegates = tomap({
-    k8s = merge(local.cristiab_lab_org_delegates.k8s)
-  })
-  harness_platform_secrets = merge(
-    local.cristian_secrets_org
-  )
+  harness_platform_organizations = var.cristian_lab_orgs
+  harness_platform_delegates     = local.cristiab_account_delegates
+  harness_platform_secrets       = local.cristian_account_secrets
 
-  harness_template_endpoint_account_args = "?accountIdentifier=${var.harness_platform_account_id}&orgIdentifier=${module.bootstrap_harness_account.organization[var.cristian_lab_org_projects.organization_name].org_id}&storeType=INLINE&comments=terraform-generated"
+  # harness_platform_connectors = merge(
+  #   local.cristiab_lab_org_connectors
+  # )
+
+  # harness_template_endpoint_account_args = "?accountIdentifier=${var.harness_platform_account_id}&orgIdentifier=${module.bootstrap_harness_account.organization[var.cristian_lab_org_projects.organization_name].org_id}&storeType=INLINE&comments=terraform-generated"
 }

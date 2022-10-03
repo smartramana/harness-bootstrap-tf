78e017b1316b6267ca688af85df037a0f2cafef2
root <cristiano.rosetti@gmail.com>
2022-10-03 04:45:48 +0000

    intial base with git connector

--- harness-provision/main.tf
+++ harness-provision/main.tf
@@ -18,6 +18,18 @@ module "bootstrap_harness_delegates" {
   harness_account_id         = var.harness_platform_account_id
 }
 
+module "bootstrap_harness_connectors" {
+  depends_on = [
+    module.bootstrap_harness_account,
+  ]
+  source                             = "git::https://github.com/crizstian/harness-terraform-modules.git//harness-connectors?ref=main"
+  harness_platform_github_connectors = local.github_connectors
+
+  providers = {
+    harness = harness.provisioner
+  }
+}
+
 output "account" {
   value = {
     organizations = module.bootstrap_harness_account.organization

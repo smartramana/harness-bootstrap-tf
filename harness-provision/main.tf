31165613f130befaf79c6884c1432e54117de5d0
Cristian Ramirez <cristiano.rosetti@gmail.com>
2022-10-08 22:10:34 -0500

    switch module to fetch from main

--- harness-provision/main.tf
+++ harness-provision/main.tf
@@ -1,6 +1,6 @@
 # Create Organizations
 module "bootstrap_harness_account" {
-  source                         = "git::https://github.com/crizstian/harness-terraform-modules.git//harness-project?ref=refactor"
+  source                         = "git::https://github.com/crizstian/harness-terraform-modules.git//harness-project?ref=main"
   suffix                         = random_string.suffix.id
   harness_platform_organizations = var.harness_platform_organizations
 
@@ -14,7 +14,7 @@ module "bootstrap_harness_delegates" {
   depends_on = [
     module.bootstrap_harness_account,
   ]
-  source                     = "git::https://github.com/crizstian/harness-terraform-modules.git//harness-delegate?ref=refactor"
+  source                     = "git::https://github.com/crizstian/harness-terraform-modules.git//harness-delegate?ref=main"
   harness_platform_delegates = var.harness_platform_delegates
   harness_platform_api_key   = var.harness_platform_api_key
   harness_account_id         = var.harness_platform_account_id
@@ -25,7 +25,7 @@ module "bootstrap_harness_connectors" {
   depends_on = [
     module.bootstrap_harness_account,
   ]
-  source                             = "git::https://github.com/crizstian/harness-terraform-modules.git//harness-connectors?ref=refactor"
+  source                             = "git::https://github.com/crizstian/harness-terraform-modules.git//harness-connectors?ref=main"
   suffix                             = random_string.suffix.id
   harness_platform_github_connectors = local.github_connectors
 
@@ -39,7 +39,7 @@ module "bootstrap_harness_pipelines" {
   depends_on = [
     module.bootstrap_harness_account,
   ]
-  source                     = "git::https://github.com/crizstian/harness-terraform-modules.git//harness-pipeline?ref=refactor"
+  source                     = "git::https://github.com/crizstian/harness-terraform-modules.git//harness-pipeline?ref=main"
   suffix                     = random_string.suffix.id
   harness_platform_pipelines = local.pipelines
 }

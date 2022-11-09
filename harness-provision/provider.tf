f57e9c7f0fd85e9c32f079e7e1a6de25e64d1f7a
root <cristiano.rosetti@gmail.com>
2022-11-09 02:49:32 +0000

    add suport for aws, docker connector, delegate and tf refactor

--- harness-provision/provider.tf
+++ harness-provision/provider.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_providers {
+    harness = {
+      source = "harness/harness"
+    }
+  }
+
+  backend "gcs" {}
+}
+
 provider "harness" {
   endpoint         = "https://app.harness.io/gateway"
   account_id       = var.harness_platform_account_id
@@ -9,16 +19,6 @@ provider "harness" {
   endpoint         = "https://app.harness.io/gateway"
   account_id       = var.harness_platform_account_id
   platform_api_key = var.harness_platform_api_key
-}
-
-terraform {
-  required_providers {
-    harness = {
-      source = "harness/harness"
-    }
-  }
-
-  backend "gcs" {}
 }
 
 resource "random_string" "suffix" {
